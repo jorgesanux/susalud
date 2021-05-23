8a4af27f2b61957099b4ebdeba63b47bcab8c989
Jorgesanux <jorgesanux1@gmail.com>
2021-05-23 00:00:57 -0500

    Agregada entidad de Supervision

---
+++ src/main/java/com/ex/susalud/model/Supervision.java
@@ -0,0 +1,57 @@
+package com.ex.susalud.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "supervision")
+@Getter
+@Setter
+public class Supervision {
+
+    private enum Tipo{FISCALIZADORA,ORIENTATIVA}
+    private enum Modalidad{INTEGRAL,SELECTIVA}
+    private enum TipoIntervencion{INTEGRAL,SELECTIVA}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_supervision")
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo",length = 20)
+    private Tipo tipo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "modalidad",length = 20)
+    private Modalidad modalidad;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_intervencion",length = 20)
+    private TipoIntervencion tipoIntervencion;
+
+    @Column(name = "fecha")
+    private LocalDate fecha;
+
+    @Column(name = "cantidad_avance")
+    private int cantidadAvance;
+
+    @Column(name = "total_avance")
+    private int totalAvance;
+
+    @OneToOne
+    @JoinColumn(name = "id_plan",referencedColumnName = "id_plan")
+    private Plan id_plan;
+
+    @OneToOne
+    @JoinColumn(name = "id_etapa",referencedColumnName = "id_etapa")
+    private Etapa id_etapa;
+
+    @OneToOne
+    @JoinColumn(name = "id_entidad",referencedColumnName = "id_entidad")
+    private Entidad id_entidad;
+
+}
