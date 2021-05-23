b32e918d16f05d1475f145e7bc45db34ef4a0d75
Jorgesanux <jorgesanux1@gmail.com>
2021-05-23 02:47:04 -0500

    Agregados repositorios

--- src/main/java/com/ex/susalud/repository/PosicionSupervisionRepository.java
+++ src/main/java/com/ex/susalud/repository/PosicionSupervisionRepository.java
@@ -1,2 +1,9 @@
-package com.ex.susalud.repository;public interface PosicionSupervisionRepository {
+package com.ex.susalud.repository;
+
+import com.ex.susalud.model.PosicionSupervision;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PosicionSupervisionRepository extends JpaRepository<PosicionSupervision,Integer> {
 }
--- src/main/java/com/ex/susalud/repository/SupervisionRepository.java
+++ src/main/java/com/ex/susalud/repository/SupervisionRepository.java
@@ -1,2 +1,9 @@
-package com.ex.susalud.repository;public interface SupervisionRepository {
+package com.ex.susalud.repository;
+
+import com.ex.susalud.model.Supervision;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface SupervisionRepository extends JpaRepository<Supervision,Integer> {
 }
