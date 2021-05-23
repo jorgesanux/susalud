90cfa083093d8a47ed94aa774521a029bf0c123a
Jorgesanux <jorgesanux1@gmail.com>
2021-05-23 02:46:33 -0500

    Agregados REST GET

--- src/main/java/com/ex/susalud/controller/MainController.java
+++ src/main/java/com/ex/susalud/controller/MainController.java
@@ -1,2 +1,14 @@
-package com.ex.susalud.controller;public class MainController {
+package com.ex.susalud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+    @GetMapping()
+    public String getMain(){
+        return "<h1>SuSalud API v1.0</h1>";
+    }
 }
--- src/main/java/com/ex/susalud/controller/SupervisionController.java
+++ src/main/java/com/ex/susalud/controller/SupervisionController.java
@@ -1,2 +1,25 @@
-package com.ex.susalud.controller;public class SupervisionController {
+package com.ex.susalud.controller;
+
+import com.ex.susalud.model.Supervision;
+import com.ex.susalud.repository.SupervisionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("supervision")
+public class SupervisionController {
+    @Autowired
+    private SupervisionRepository supervisionRepository;
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<Supervision>> getAllSupervisiones(){
+        return new ResponseEntity<>(supervisionRepository.findAll(), HttpStatus.OK);
+    }
 }
